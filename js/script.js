9c5f82ae2e886fe6311a1fbcb79161f688ab5ae9
Aimanreyaz <109452985+Aimanreyaz@users.noreply.github.com>
2024-04-30 22:44:47 +0600

    fatch data

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const loadData =async() =>{
+    const url = ` https://openapi.programming-hero.com/api/ai/tools`
+    const res = await fetch(url);
+    const data = await res.json();
+    displayData(data.data.tools)
+}
+const displayData =allData =>{
+
+    console.log(allData)
+}  
+loadData();
