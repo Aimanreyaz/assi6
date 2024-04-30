c6fd4551ee676687260da364e0407efabc8751b4
Aimanreyaz <109452985+Aimanreyaz@users.noreply.github.com>
2024-04-30 23:29:25 +0600

    add tools image

--- js/script.js
+++ js/script.js
@@ -4,8 +4,32 @@ const loadData =async() =>{
     const data = await res.json();
     displayData(data.data.tools)
 }
-const displayData =allData =>{
-
-    console.log(allData)
+const displayData =tools =>{
+    console.log(tools)
+    const toolContainer = document.getElementById('tools-container');
+    tools.forEach(tool=>{
+        const toolsDiv=document.createElement('div');
+        toolsDiv.classList.add('col');
+        toolsDiv.innerHTML=`
+        <div class="card h-100">
+        <img src="${tool.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Features</h5>
+          <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+          
+          </ul>
+        </div>
+        <div class="card-footer">
+          <small class="text-body-secondary">Last updated 3 mins ago</small>
+        </div>
+      </div>
+        
+        `  
+        toolContainer.appendChild(toolsDiv);
+    })
+    
 }  
 loadData();
